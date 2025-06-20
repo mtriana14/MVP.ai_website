87a89ad76f077dd1b8eeb089e1e8a32951405b35
mtriana14 <159511457+mtriana14@users.noreply.github.com>
2025-06-19 20:44:59 -0400

    Update route.ts

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 import { NextResponse } from "next/server";
-import { EmailTemplate } from "../../../../utils/emailTemplate";
+import { EmailTemplate } from "../../../utils/emailTemplate";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const SENDER_EMAIL = "mvp.aiceo@gmail.com";
