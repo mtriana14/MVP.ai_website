050e7bec10e92a87173ebbe285177f2dc3ddbd64
mtriana14 <159511457+mtriana14@users.noreply.github.com>
2025-07-16 14:40:13 -0400

    Update page.js
    Renamed file from companionguide.jsx to CompanionGuide.jsx

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Header from "../app/component/header";
 import Mvp from "../app/component/mvp";
 import Features from "../app/component/feature";
-import CompanionGuide from "./component/companionguide";
+import CompanionGuide from "./component/CompanionGuide";
 function Home() {
   return (
     <>
